0500d8061e99352b88833dbb4d27e257c2e2d964
Osiozekhai Aliu <oaliu@hotmail.com>
2024-12-23 13:30:19 +0100

    Refactor main.py to enhance API response handling and update model in payload; clean up docker-compose.yml

--- src/main.py
+++ src/main.py
@@ -8,21 +8,25 @@ headers = {
 }
 
 payload = {
+    # "model": "jimscard/devopd:latest",
+    # "model": "gemma2:latest",
     "model": "llama3.2:latest",
-    "prompt": "What is water made of?"
+    "prompt": "What is water made of?",
+    "stream": False,
 }
 
 try:
-    response = requests.post(url, headers=headers,  data=json.dumps(payload))
+    response = requests.post(url, headers=headers, data=json.dumps(payload))
     response.raise_for_status()  # Raise an exception for HTTP errors
-    
-    # Debug raw content
-    print("Raw Response Content:", response.json)
     
     # Try parsing JSON
     try:
-        data = response.json()
-        print("JSON Response:", data)
+        result = json.loads(response.text)
+
+        print('Model: '+payload['model'])
+        print('Prompt: '+payload['prompt'])
+        print('Result')
+        print(result['response'])
     except ValueError:
         print("Response is not valid JSON. Raw response:", response.text)
 except requests.exceptions.RequestException as e:
